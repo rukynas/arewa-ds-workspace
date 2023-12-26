c1de1bcad3a9dfa41506236eb0aaf77239eeb47a
rukynas <rukynas08@gmail.com>
2023-12-26 17:19:40 +0100

    my day 2

---
+++ day_2/variables.py
@@ -0,0 +1,12 @@
+#Day 2: 30 Days of python programming
+first_name = "Ruqayya"
+last_name = "Nasir"
+full_name = first_name + " " + last_name
+country = "Nigeria"
+city = "Kano"
+age = 32
+year = 2023
+is_married = True
+is_true = True
+is_light_on = True
+
