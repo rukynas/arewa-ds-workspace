83f49738b3a495efee475af11d506ec8305284ec
rukynas <rukynas08@gmail.com>
2023-12-26 20:16:47 +0100

    my day 1

---
+++ Day1/helloworld.py
@@ -0,0 +1,28 @@
+#SUBTRACTION
+print(3 - 4)
+#MULTIPLICATION
+print(3 * 4)
+#MODULUS
+print(3 % 4)
+#DIVISION
+print(3/4)
+#EXPONENTIAL
+print(3**4)
+#floor division operator
+print(3//4)
+
+#NUMBER 3
+print('Ruqayya')
+print("Nasir Iro")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+
+#NUMBER 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Ruqayya'))
+print(type("Nasir Iro"))
+print(type("Nigeria"))
