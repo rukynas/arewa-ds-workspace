3fcefca6f135874be9964ca76a98d03a18efa467
rukynas <rukynas08@gmail.com>
2024-01-08 14:25:23 +0100

    my day 15

---
+++ day_15/day15_assignment.py
@@ -0,0 +1,19 @@
+print('hello world')
+age = 25
+print(age)
+numbers = [1, 2, 3, 4, 5]
+numbers[5]
+
+import math
+math.pi
+
+user = {'name':'Asab', 'age':250, 'country':'Finland'}
+user['name']
+
+4 + int('3')
+4 + float('3')
+
+from math import pow
+pow(2,3)
+
+1/0
